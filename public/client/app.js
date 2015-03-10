f76b2249ba61cfabf45101e397d7b234fd23bf05
Brian Hsu <brianhsu@Brians-MacBook-Pro-2.local>
2015-03-09 21:59:24 -0700

    Made API call to acquire user's info with oauth.

--- public/client/app.js
+++ public/client/app.js
@@ -1,24 +1,39 @@
+var app;
+
 $(function(){
-  $('#findUser').submit(function(e){
-    var userhandle = $('#githubhandle').val();
-
-    e.preventDefault();
-    $.ajax({
-      url: "https://api.github.com/users/" + userhandle,
-      type: "GET"
-    }).done(function(data){
-      console.log(data);
-      var followers = JSON.parse(data.followers_url);
-      var following = JSON.parse(data.following_url);
-      var friends = [];
-      for (var i = 0; i< followers.length; i++){
-        friends.push(followers[i][login]);
-      }
-      for (var i = 0; i< following.length; i++){
-        friends.push(followers[i][login]);
-      }
-      console.log(friends);
-    });
-
-  });
+  app = {
+    server: "https://api.github.com/users/",
+    
+    init: function(){
+      app.$findUser = $('#submit');
+      app.$findUser.on('click', app.handleSubmit);
+    },
+    getInfo: function(username){
+      $.ajax({
+        data: username,
+        url: "/getInfo",
+        type: "GET",
+        contentType: 'application/json',
+        success: function(data){
+          console.dir(data);
+          // var followers = JSON.parse(data.followers_url);
+          // var following = JSON.parse(data.following_url);
+          // var friends = [];
+          // for (var i = 0; i< followers.length; i++){
+          //   friends.push(followers[i][login]);
+          // }
+          // for (var i = 0; i< following.length; i++){
+          //   friends.push(followers[i][login]);
+          // }
+          // console.log(friends);
+        }
+      });
+    },
+    handleSubmit:function(event) {
+      var username = $('#githubhandle').val();
+      app.getInfo(username);
+      // Stop the form from submitting
+      event.preventDefault();
+    },
+  }
 });
--- server.js
+++ server.js
@@ -3,49 +3,59 @@ var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var expressSession = require('express-session');
 var cookieParser = require('cookie-parser');
-var passport = require('passport');
-var GithubStrategy = require('passport-github').Strategy;
 var githubcode = require('./githubapp.js');
-var auth = require('./auth.js');
+
+var cors = require('cors');
+var Github = require('github-api');
 
 var app = express();
+app.use(cors());
 app.use(cookieParser());
 app.use(partials());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.use(expressSession({secret:'GithubsagabyBrian'}));
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.use(new GithubStrategy({
-  clientID: githubcode.clientID,
-  clientSecret: githubcode.secret,
-  callbackURL: 'http://127.0.0.1:5678/auth/callback'
-}, function(accessToken, refreshToken, profile, done){
-  done(null, {
-    accessToken: accessToken,
-    profile: profile
+var github = new Github({
+  token: githubcode.token,
+  auth: 'oauth'
+});
+
+var user = github.getUser();
+var url= "https://api.github.com/users/";
+
+var handleGet = function(request, response){
+  var url = request.url.toString();
+  var username = url.substring(url.indexOf("?")+1);
+  console.log("Should have username: ", username );
+
+  // request(url + username).pipe(fs.createWriteStream(exports.paths.archivedSites + "/" + url));
+  
+  user.show(username, function(err, data) {
+    if (err){ console.log(err)}
+    console.dir(data.followers);
+
+    // var followers = JSON.parse(data.followers_url);
+    // var following = JSON.parse(data.following_url);
+    // var friends = [];
+    // var test;
+    // var friendlist = app.get(data.followers_url, function(req, res){
+    //    http.get(data.followers_url, function(data) {
+    //       console.log("friends");
+    //       console.log(data);
+    //       res.json(data);
+    //    });
+    // });
+    // for (var i = 0; i< followers.length; i++){
+    //   friends.push(followers[i][login]);
+    // }
+    // for (var i = 0; i< following.length; i++){
+    //   friends.push(followers[i][login]);
+    // }
+    // console.log(friends);
   });
-}));
+  response.send(user);
+};
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-app.get('/auth', passport.authenticate('github'));
-app.get('/auth/error', auth.error);
-app.get('/auth/callback',
-  passport.authenticate('github', {failureRedirect: '/auth/error'}),
-  auth.callback
-);
-
-var handleGet = function(reqest, response){
-  response.send({results: Object})
-}
-
+app.get("/getInfo", handleGet);
 
 app.listen(5678);
